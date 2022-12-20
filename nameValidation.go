b095d49f3e0fab278165f6bd443875fcf09a7225
unknown <vasuchauhan9897@gmail.com>
2022-12-20 15:32:52 +0530

    NameValidation : Adds Name validation based on length

--- nameValidation.go
+++ nameValidation.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
+// this function checks name validation based on lengths.
 func isValidName(name string ) bool {
 	name = strings.TrimSpace(name)
 	if len(name)>=5 && len(name)<=15{
@@ -13,11 +14,15 @@ func isValidName(name string ) bool {
 	return false
 }
 
-func main() {
-	name := "vasu chauhan"
+func print(name string) {
 	if isValidName(name){
 		fmt.Println(fmt.Sprintf("%s is a valid name",name))
 	}else{
 		fmt.Println(fmt.Sprintf("%s is not a valid name",name))
 	}
+}
+
+func main() {
+	print("vasu chauhan")
+	print("invalidnameinvalidnameinvalidname")
 }
